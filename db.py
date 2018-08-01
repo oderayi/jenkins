98564a6f1a05b983e13624c3aba0ffff29802607
Steven Oderayi <soderayi@ab-mfbnigeria.com>
2018-08-01 09:29:15 +0100

    Update db

--- db.py
+++ db.py
@@ -1,52 +1,54 @@
 import sqlite3
 
-db_name = "jenkins.db"
+
+class db:
+
+  db_name = "jenkins.db"
+
+  def insert(conn, row):
+      conn.execute('insert into jobs (job_name, date_checked) values (?, ?)',
+                (row['job_name'], row['date_checked']))
+      conn.commit()
+      conn.close()
 
 
-def insert(db, row):
-    db.execute('insert into jobs (job_name, date_checked) values (?, ?)',
-               (row['job_name'], row['date_checked']))
-    db.commit()
-    db.close()
+  def retrieve(conn, job_name):
+      cursor = conn.execute('select * from jobs where job_name = ?', (job_name,))
+      record = cursor.fetchone()
+      conn.close()
+      return record
 
 
-def retrieve(db, job_name):
-    cursor = db.execute('select * from jobs where job_name = ?', (job_name,))
-    record = cursor.fetchone()
-    db.close()
-    return record
+  def update(conn, row):
+      conn.execute('update jobs set date_checked = ? where job_name = ?',
+                (row['date_checked'], row['job_name']))
+      conn.commit()
+      conn.close()
 
 
-def update(db, row):
-    db.execute('update jobs set date_checked = ? where job_name = ?',
-               (row['date_checked'], row['job_name']))
-    db.commit()
-    db.close()
+  def delete(conn, job_name):
+      conn.execute('delete from jobs where job_name = ?', (job_name,))
+      conn.commit()
+      conn.close()
 
 
-def delete(db, job_name):
-    db.execute('delete from jobs where job_name = ?', (job_name,))
-    db.commit()
-    db.close()
+  def disp_rows(conn):
+      cursor = conn.execute('select * from jobs order by job_name')
+      for row in cursor:
+          print('{}: {}'.format(row['job_name'], row['date_checked']))
+      conn.close()
 
 
-def disp_rows(db):
-    cursor = db.execute('select * from jobs order by job_name')
-    for row in cursor:
-        print('{}: {}'.format(row['job_name'], row['date_checked']))
-    db.close()
+  def create_tables():
+      conn = init()
+      conn.row_factory = sqlite3.Row
+      print('Creating table: jobs (if it does not already exist:')
+      conn.execute('create table if not exists jobs ( id integer primary key autoincrement, job_name text, date_checked datetime )')
+      conn.commit()
+      conn.close()
 
 
-def create_tables():
-    db = init()
-    db.row_factory = sqlite3.Row
-    print('Creating table: jobs (if it does not already exist:')
-    db.execute('create table if not exists jobs ( id integer primary key autoincrement, job_name text, date_checked datetime )')
-    db.commit()
-    db.close()
-
-
-def init():
-  db = sqlite3.connect(db_name)
-  create_tables()
-  return db
+  def init():
+      conn = sqlite3.connect(db_name)
+      create_tables()
+      return conn
--- list_jobs.py
+++ list_jobs.py
@@ -4,9 +4,17 @@ import sys, jenkins, db
 from datetime import datetime
 
 def listJobs(addr):
-  print(f"Jobs on: {addr}\n")
   server = jenkins.Jenkins(addr)
-  print(server.jobs_count())
+  jobs = server.get_jobs()
+  print(f"Jobs on: {addr}\n")
+  print(jobs)
+  storeJobs(jobs)
+
+def storeJobs(jobs):
+  dbHandle = db()
+  conn = db.init()
+  for job in jobs
+    db.insert(conn, [job.name, datetime.now])
 
 
 if __name__ == "__main__":
