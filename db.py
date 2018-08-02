03933fe925e77bbf6710338c9fd9fb17c7399aac
Steven Oderayi <soderayi@ab-mfbnigeria.com>
2018-08-02 13:36:25 +0100

    Update

--- db.py
+++ db.py
@@ -5,41 +5,41 @@ class db:
 
   db_name = "jenkins.db"
 
-  def insert(conn, row):
+  def insert(self, conn, row):
       conn.execute('insert into jobs (job_name, job_status, date_checked) values (?, ?, ?)',
                    (row['job_name'], row['job_status'], row['date_checked']))
       conn.commit()
       conn.close()
 
 
-  def retrieve(conn, job_name):
+  def retrieve(self, conn, job_name):
       cursor = conn.execute('select * from jobs where job_name = ?', (job_name,))
       record = cursor.fetchone()
       conn.close()
       return record
 
 
-  def update(conn, row):
+  def update(self, conn, row):
       conn.execute('update jobs set date_checked = ?, job_status = ? where job_name = ?',
                    (row['date_checked'], row['job_status'], row['job_name']))
       conn.commit()
       conn.close()
 
 
-  def delete(conn, job_name):
+  def delete(self, conn, job_name):
       conn.execute('delete from jobs where job_name = ?', (job_name,))
       conn.commit()
       conn.close()
 
 
-  def disp_rows(conn):
+  def disp_rows(self, conn):
       cursor = conn.execute('select * from jobs order by job_name')
       for row in cursor:
           print('{}: {} {}'.format(row['job_name'], row['job_status'], row['date_checked']))
       conn.close()
 
 
-  def create_tables():
+  def create_tables(self):
       conn = init()
       conn.row_factory = sqlite3.Row
       print('Creating table: jobs (if it does not already exist:')
@@ -48,7 +48,7 @@ class db:
       conn.close()
 
 
-  def init():
+  def init(self):
       conn = sqlite3.connect(db_name)
       create_tables()
       return conn
